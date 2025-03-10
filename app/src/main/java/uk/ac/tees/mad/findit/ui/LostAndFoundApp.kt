2774a01bc2576db24e9be3b1b910b1a878b18dea
Chaitanya <findit.tees@gmail.com>
2025-03-10 12:00:00 +0530

    Implemented jetpack navigation with navigation graph

--- app/src/main/java/uk/ac/tees/mad/findit/ui/LostAndFoundApp.kt
+++ app/src/main/java/uk/ac/tees/mad/findit/ui/LostAndFoundApp.kt
@@ -2,19 +2,23 @@ package uk.ac.tees.mad.findit.ui
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import uk.ac.tees.mad.findit.navigation.Routes
 import uk.ac.tees.mad.findit.ui.screens.auth.AuthScreen
+import uk.ac.tees.mad.findit.ui.screens.home.HomeScreen
 import uk.ac.tees.mad.findit.ui.screens.splash.SplashScreen
 
 @Composable
 fun LostAndFoundApp(navController: NavHostController = rememberNavController()) {
     NavHost(navController = navController, startDestination = Routes.SPLASH) {
         // Navigation graph
-        composable(Routes.SPLASH) {
+        composable(route = Routes.SPLASH) {
             SplashScreen(
                 onNavigateToAuth = {
                     navController.navigate(Routes.AUTH) {
@@ -28,7 +32,9 @@ fun LostAndFoundApp(navController: NavHostController = rememberNavController()) 
                 }
             )
         }
-        composable(Routes.AUTH) {
+
+
+        composable(route = Routes.AUTH) {
             AuthScreen(
                 onNavigateToHome = {
                     navController.navigate(Routes.HOME) {
@@ -37,9 +43,31 @@ fun LostAndFoundApp(navController: NavHostController = rememberNavController()) 
                 }
             )
         }
-
-        composable(Routes.HOME) {
-            Text("Home Screen Placeholder")
+        composable(route = Routes.HOME) {
+            HomeScreen(
+                onNavigateToItemDetail = { itemId ->
+                    navController.navigate("${Routes.ITEM_DETAIL}/$itemId")
+                },
+                onNavigateToNewItem = {
+                    navController.navigate(Routes.NEW_ITEM)
+                },
+                onNavigateToProfile = {
+                    navController.navigate(Routes.PROFILE)
+                }
+            )
+        }
+        composable(
+            route = "${Routes.ITEM_DETAIL}/{itemId}",
+            arguments = listOf(navArgument("itemId") { type = NavType.StringType })
+        ) { backStackEntry ->
+            val itemId = backStackEntry.arguments?.getString("itemId") ?: ""
+            // ItemDetailScreen implementation
+        }
+        composable(route = Routes.PROFILE) {
+            // ProfileScreen implementation
+        }
+        composable(route = Routes.NEW_ITEM) {
+            // NewItemScreen implementation
         }
     }
 }
---
+++ app/src/main/java/uk/ac/tees/mad/findit/ui/screens/home/HomeScreen.kt
@@ -0,0 +1,15 @@
+package uk.ac.tees.mad.findit.ui.screens.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import uk.ac.tees.mad.findit.navigation.Routes
+
+@Composable
+fun HomeScreen(
+    onNavigateToItemDetail: (String) -> Unit,
+    onNavigateToNewItem: () -> Unit,
+    onNavigateToProfile: () -> Unit
+) {
+
+}
+
